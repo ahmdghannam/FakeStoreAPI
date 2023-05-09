e98877c5b145a4a684a540635ddc25e07ef9c1db
Ahmed Ghannam <iia7mt@gmail.com>
2023-05-09 18:09:16 +0300

    add base fragment and base view model

---
+++ app/src/main/java/com/example/fakestoreapi/view/base_classes/BaseFragment.kt
@@ -0,0 +1,28 @@
+package com.example.fakestoreapi.view.base_classes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<DB : ViewDataBinding, VM : BaseViewModel> : Fragment() {
+    private var _binding: DB? = null
+    val binding: DB
+        get() = _binding as DB
+    protected abstract val viewModel: VM
+    protected abstract val layoutIdFragment: Int
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutIdFragment, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/fakestoreapi/view/base_classes/BaseViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.fakestoreapi.view.base_classes
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+
+abstract class BaseViewModel : ViewModel() {
+    protected val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+
+    protected fun Disposable.addTo(){
+        compositeDisposable.add(this)
+    }
+}
