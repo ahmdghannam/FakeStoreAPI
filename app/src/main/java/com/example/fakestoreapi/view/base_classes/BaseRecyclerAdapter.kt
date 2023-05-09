0642c3cafa2988ef6c6cf1bf14d8847db7b8e06d
Ahmed Ghannam <iia7mt@gmail.com>
2023-05-09 21:48:12 +0300

    create base view holder

---
+++ app/src/main/java/com/example/fakestoreapi/view/base_classes/BaseRecyclerAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.fakestoreapi.view.base_classes
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fakestoreapi.BR
+
+abstract class BaseRecyclerAdapter<T>(
+    private var items: List<T>,
+    private var listener: BaseInteractionListener
+) : RecyclerView.Adapter<BaseRecyclerAdapter<T>.BaseViewHolder>() {
+
+    abstract val layoutId: Int
+
+    interface BaseInteractionListener
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return OneItemViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                layoutId,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        if (holder is OneItemViewHolder) {
+            holder.binding.setVariable(BR.item, items[position])
+            holder.binding.setVariable(BR.listener, listener)
+        }
+    }
+
+    fun getItems() = items
+    fun setItems(newItems: List<T>) {
+        val diffUtil = DiffUtil.calculateDiff(MyDiffUtils(items, newItems))
+        items = newItems
+        diffUtil.dispatchUpdatesTo(this)
+    }
+
+    inner class OneItemViewHolder(val binding: ViewDataBinding) : BaseViewHolder(binding)
+
+    abstract inner class BaseViewHolder(binding: ViewDataBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    inner class MyDiffUtils<T>(private val oldList: List<T>, private val newList: List<T>) :
+        DiffUtil.Callback() {
+        override fun getOldListSize() = oldList.size
+        override fun getNewListSize() = newList.size
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) = true
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition] == newList[newItemPosition]
+        }
+
+    }
+}
