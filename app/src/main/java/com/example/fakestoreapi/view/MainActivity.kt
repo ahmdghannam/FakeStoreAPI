5887640e9064c262eeac54e4ae025134bc0efc62
Ahmed Ghannam <iia7mt@gmail.com>
2023-05-12 12:13:13 +0300

    customize the rating bar

--- app/src/main/java/com/example/fakestoreapi/view/MainActivity.kt
+++ app/src/main/java/com/example/fakestoreapi/view/MainActivity.kt
@@ -5,13 +5,14 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.fakestoreapi.R
 import com.example.fakestoreapi.view.login.LoginFragment
+import com.example.fakestoreapi.view.product_details.ProductDetailsFragment
 import com.example.fakestoreapi.view.profile.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        replaceFragment(ProfileFragment())
+        replaceFragment(ProductDetailsFragment())
     }
 
     private fun replaceFragment(fragment: Fragment) {
