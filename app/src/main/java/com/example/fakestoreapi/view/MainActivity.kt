2d611027ebfcef94a880161bd539c4baa256ee16
Ahmed Ghannam <iia7mt@gmail.com>
2023-05-31 12:18:58 +0300

    create menu, import icons, link navigation component to activity, create navigation file

--- app/src/main/java/com/example/fakestoreapi/view/MainActivity.kt
+++ app/src/main/java/com/example/fakestoreapi/view/MainActivity.kt
@@ -1,32 +1,39 @@
 package com.example.fakestoreapi.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.fakestoreapi.R
+import com.example.fakestoreapi.databinding.ActivityMainBinding
 import com.example.fakestoreapi.model.local.SharedPreferencesUtil
 import com.example.fakestoreapi.model.local.StoreDataBase
-import com.example.fakestoreapi.view.cart.CartFragment
-import com.example.fakestoreapi.view.categories.CategoriesFragment
-import com.example.fakestoreapi.view.home.HomeFragment
-import com.example.fakestoreapi.view.login.LoginFragment
-import com.example.fakestoreapi.view.product_details.ProductDetailsFragment
-import com.example.fakestoreapi.view.profile.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         val sharedPreferencesUtil = SharedPreferencesUtil(applicationContext)
         sharedPreferencesUtil.userId = 1
         StoreDataBase.init(applicationContext)
-        replaceFragment(ProductDetailsFragment())
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragmentContainer, fragment)
-        transaction.commit()
+    override fun onResume() {
+        super.onResume()
+        val navController = findNavController(R.id.fragmentContainer)
+//        NavigationUI.setupActionBarWithNavController(this,navController)
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainer)
+        navController.navigateUp()
+        return true
     }
 }
 
